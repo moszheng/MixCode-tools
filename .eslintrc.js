38fa7e7339d07a04b4223b0458f9c9c95ea55865
Mos Zheng <47658172+moszheng@users.noreply.github.com>
2024-03-19 19:41:32 +0800

    [ + ] Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "eslint:recommended",
+    'extends': [
+        'google',
+        "prettier",
+    ],
     "overrides": [
         {
             "env": {
